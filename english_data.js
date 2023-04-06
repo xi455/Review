50ae886db76b7a0d5ecedfc80beae28a9f34bea4
Hung_xi <a3083493@gmail.com>
2023-04-06 16:57:02 +0800

    添加英文測驗功能

--- english_data.js
+++ english_data.js
@@ -1,96 +1,112 @@
 let Data = {
     "suburb": {
         "範圍": "6A",
+        "全部":"All",
         "中文": "郊區",
         "例句": "I grew up in the suburbs where everything was quiet and green.",
         "句子的中文": "我在郊區長大，那裡的一切都是安靜和綠色的。"
     },
     "resisting": {
         "範圍": "6A",
+        "全部":"All",
         "中文": "反抗",
         "例句": "She was resisting arrest and had to be restrained.",
         "句子的中文": "她在反抗逮捕，必須被制止。"
     },
     "grasps": {
         "範圍": "6A",
+        "全部":"All",
         "中文": "抓牢",
         "例句": "He grasps the concept of quantum mechanics quickly.",
         "句子的中文": "他很快就掌握了量子力學的概念。"
     },
     "sustainable": {
         "範圍": "6A",
+        "全部":"All",
         "中文": "適當的",
         "例句": "We need to develop sustainable energy sources to protect the environment.",
         "句子的中文": "我們需要開發可持續的能源來保護環境。"
     },
     "chief": {
         "範圍": "6A",
+        "全部":"All",
         "中文": "主要的/最重要的",
         "例句": "The chief reason for the company's success is its innovative products.",
         "句子的中文": "公司成功的主要原因是其創新產品。"
     },
     "get_on_board": {
         "範圍": "6A",
+        "全部":"All",
         "中文": "加入",
         "例句": "If you want to succeed, you need to get on board with the latest trends.",
         "句子的中文": "如果你想成功，你需要跟上最新的潮流。"
     },
     "devastating": {
         "範圍": "6A",
+        "全部":"All",
         "中文": "毀滅性的",
         "例句": "The earthquake had a devastating effect on the local community.",
         "句子的中文": "地震對當地社區產生了毀滅性的影響。"
     },
     "vibrant": {
         "範圍": "6A",
+        "全部":"All",
         "中文": "活躍的",
         "例句": "The city's downtown is always vibrant with life and activity.",
         "句子的中文": "城市的市中心總是充滿生氣和活力。"
     },
     "complicated": {
         "範圍": "6A",
+        "全部":"All",
         "中文": "複雜的",
         "例句": "The instructions for this DIY project are quite complicated.",
         "句子的中文": "這個自己動手的項目的說明相當複雜。"
     },
     "at_once": {
         "範圍": "6A",
+        "全部":"All",
         "中文": "立刻/馬上",
         "例句": "Please call me back at once.",
         "句子的中文": "請馬上回電給我。"
     },
     "withdraw": {
         "範圍": "6B",
+        "全部":"All",
         "中文": "離開/撤退",
         "例句": "He decided to withdraw from the race.",
         "句子的中文": "他決定退出比賽。"
     },
     "agitation": {
         "範圍": "6B",
+        "全部":"All",
         "中文": "激動/不安",
         "例句": "The news caused great agitation among the public.",
         "句子的中文": "這個消息引起了公眾的極大不安。"
     },
     "evade": {
         "範圍": "6B",
+        "全部":"All",
         "中文": "躲避/逃避",
         "例句": "He tried to evade the question.",
         "句子的中文": "他試圖逃避這個問題。"
     },
     "instinct": {
         "範圍": "6B",
+        "全部":"All",
         "中文": "本能/天性",
         "例句": "A mother's instinct to protect her child is very strong.",
         "句子的中文": "母親保護孩子的本能非常強烈。"
     },
     "genuine": {
         "範圍": "6B",
+        "全部":"All",
         "中文": "真的/名副其實的",
         "例句": "I can assure you that this is a genuine antique.",
         "句子的中文": "我向你保證這是一件名副其實的古董。"
     },
     "weapons": {
         "範圍": "6B",
+        "全部":"All",
         "中文": "武器",
         "例句": "The soldiers were equipped with the latest weapons.",
         "句子的中文": "士兵們裝備了最新的武器。"
@@ -98,72 +114,84 @@ let Data = {
     },
     "compiled": {
         "範圍": "6B",
+        "全部":"All",
         "中文": "編輯",
         "例句": "The code needs to be compiled before it can be run.",
         "句子的中文": "在運行之前需要編譯代碼。"
     },
     "chaos": {
         "範圍": "6B",
+        "全部":"All",
         "中文": "混亂",
         "例句": "The party turned into chaos after the unexpected arrival of the police.",
         "句子的中文": "警察的意外到來導致派對陷入混亂。"
     },
     "collapse": {
         "範圍": "6B",
+        "全部":"All",
         "中文": "瓦解",
         "例句": "The old building collapsed after the earthquake.",
         "句子的中文": "這座老建築在地震後倒塌了。"
     },
     "abolish": {
         "範圍": "6B",
+        "全部":"All",
         "中文": "廢除/禁止",
         "例句": "The government decided to abolish the law that restricted freedom of speech.",
         "句子的中文": "政府決定廢除限制言論自由的法律。"
     },
     "progresses": {
         "範圍": "7A",
+        "全部":"All",
         "中文": "進行/前進",
         "例句": "The construction of the new school is progressing smoothly.",
         "句子的中文": "新學校的建設進展順利。"
     },
     "sacred": {
         "範圍": "7A",
+        "全部":"All",
         "中文": "神聖的",
         "例句": "The temple is a sacred place for the local people.",
         "句子的中文": "這座寺廟對當地人來說是個神聖的地方。"
     },
     "harmony": {
         "範圍": "7A",
+        "全部":"All",
         "中文": "和諧",
         "例句": "The family lived together in harmony despite their differences.",
         "句子的中文": "儘管有所不同，這個家庭還是和睦相處。"
     },
     "organized": {
         "範圍": "7A",
+        "全部":"All",
         "中文": "有條理的/井井有條的",
         "例句": "She is a very organized person and always has a plan.",
         "句子的中文": "她是個非常有條理的人，總是有計劃。"
     },
     "suspect": {
         "範圍": "7A",
+        "全部":"All",
         "中文": "懷疑",
         "例句": "The police suspect that he is involved in the robbery.",
         "句子的中文": "警方懷疑他涉嫌搶劫案。"
     },
     "society": {
         "範圍": "7A",
+        "全部":"All",
         "中文": "社會",
         "例句": "The changes in technology have transformed society.",
         "句子的中文": "技術的變革改變了社會。"
     },
     "prospective": {
         "範圍": "7A",
+        "全部":"All",
         "中文": "預期的",
         "例句": "I am excited about my prospective job.",
         "句子的中文": "我對我的預期工作感到興奮。"
     },
     "evident": {
         "範圍": "7A",
+        "全部":"All",
         "中文": "明顯的",
         "例句": "It is evident that she has worked hard on this project.",
         "句子的中文": "顯然她在這個項目上努力了。"
