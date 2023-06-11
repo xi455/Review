04b3ad66ddda4b0c7496e1cf6ee69f04ba78d3ed
hung_xi mac <a3083493@gmail.com>
2023-06-11 15:50:19 +0800

    update english data

--- english_data.js
+++ english_data.js
@@ -1,282 +1,242 @@
-let Data = {
+Data = {
     "compelling": {
         "範圍": "8A",
         "全部": "All",
         "中文": "強制的/令人注目的/令人感嘆的",
-        "例句": "The movie had a compelling storyline that kept me engaged throughout.",
-        "句子的中文": "這部電影有一個引人入勝的故事情節，讓我全程投入其中。"
+        "單字來由解釋": "compelling (動詞或形容詞) 源自拉丁詞彙compellere，由com- (意為“完全”或“強調”) 和pellere (意為“推”或“驅使”) 組成。表示強制性的、引人注目的或令人感嘆的。",
     },
     "dispute": {
         "範圍": "8A",
         "全部": "All",
         "中文": "爭論/爭執",
-        "例句": "They are in a dispute over the ownership of the land.",
-        "句子的中文": "他們在土地所有權上有爭議。"
+        "單字來由解釋": "dispute（名詞或動詞）源自拉丁詞彙disputare，由dis-（表示“相反”或“分離”）和putare（意為“計算”或“思考”）組成。表示對於某個問題、觀點或主張存在分歧，雙方進行辯論或爭執的行為或狀態。",
     },
     "proponent": {
         "範圍": "8A",
         "全部": "All",
         "中文": "提議人/擁護者",
-        "例句": "He is a strong proponent of renewable energy.",
-        "句子的中文": "他是可再生能源的堅定擁護者。"
+        "單字來由解釋": "proponent (名詞) 源自拉丁詞彙proponere，由pro- (意為“向前”或“提前”) 和ponere (意為“放置”或“提出”) 組成。表示一個提議或支持某事物的人。",
     },
     "relentlessly": {
         "範圍": "8A",
         "全部": "All",
         "中文": "無情地/殘酷地",
-        "例句": "She pursued her goals relentlessly, never giving up.",
-        "句子的中文": "她不懈追求自己的目標，從不放棄。"
-    },
-    "symptom": {
-        "範圍": "8A",
-        "全部": "All",
-        "中文": "症狀/徵候",
-        "例句": "Fever is a common symptom of the flu.",
-        "句子的中文": "發燒是流感的常見症狀。"
-    },
+        "單字來由解釋": "relentlessly（副詞）由relentless（形容詞）衍生而來。relentless源自拉丁詞彙relentare，由re-（表示“減輕”或“變溫和”）和lentus（意為“柔軟”或“溫和”）組成。表示無情地或殘酷地進行某個行動或持續某種狀態。",
+        },
+        "symptom": {
+            "範圍": "8A",
+            "全部": "All",
+            "中文": "症狀/徵候",
+            "單字來由解釋": "symptom (名詞) 源自希臘詞彙symptōma，由sym-（表示“共同的”或“一起”）和ptōma（意為“特徵”或“經歷”）組成。表示身體或心理狀態的特定症狀或徵候。",
+            },
     "disrupt": {
         "範圍": "8A",
         "全部": "All",
         "中文": "使分裂/使瓦解",
-        "例句": "The construction noise disrupted my concentration.",
-        "句子的中文": "施工的噪音打擾了我的專注力。"
+        "單字來由解釋": "disrupt (動詞) 源自拉丁詞彙disrumpere，由dis- (意為“分開”或“分散”) 和rumpere (意為“破裂”或“打破”) 組成。表示使某事物分裂或瓦解，破壞原有的結構或秩序。",
     },
     "cease": {
         "範圍": "8A",
         "全部": "All",
         "中文": "停止/終止",
-        "例句": "The rain finally ceased, and the sun came out.",
-        "句子的中文": "雨終於停了，太陽出來了。"
+        "單字來由解釋": "cease (動詞) 源自拉丁詞彙cessare，由cedere (意為“去”或“停止”) 衍生而來。表示停止或終止某個行動、狀態或存在。",
     },
     "revenge": {
         "範圍": "8A",
         "全部": "All",
         "中文": "報仇/報復",
-        "例句": "He sought revenge on those who had wronged him.",
-        "句子的中文": "他尋求對那些冤枉他的人報復。"
+        "單字來由解釋": "revenge (名詞或動詞) 源自拉丁詞彙revenire，由re- (意為“再次”) 和venire (意為“來”) 組成。表示以相同或類似的行動作為報復或回應。",
     },
     "inconclusive": {
         "範圍": "8A",
         "全部": "All",
         "中文": "非決定性的/不確定的",
-        "例句": "The results of the experiment were inconclusive.",
-        "句子的中文": "這個實驗的結果並不確定。"
+        "單字來由解釋": "inconclusive (形容詞) 由in- (意為“不”) 和conclusive (意為“決定性的”) 組成。表示無法下定論或確定的，不明確的狀態。",
     },
     "grand": {
         "範圍": "8A",
         "全部": "All",
         "中文": "雄偉的/堂皇的",
-        "例句": "The hotel lobby had a grand entrance with chandeliers and marble floors.",
-        "句子的中文": "酒店大堂有著華麗的入口，有吊燈和大理石地板。"
+        "單字來由解釋": "grand (形容詞) 源自法語詞彙grand，意為“大”或“崇高”。表示具有壯麗、堂皇或崇高的特點或品質。",
     },
     "suspicious": {
         "範圍": "8B",
         "全部": "All",
         "中文": "猜疑的/疑心的",
-        "例句": "I find his behavior very suspicious.",
-        "句子的中文": "我覺得他的行為非常可疑。"
+        "單字來由解釋": "suspicious (形容詞) 源自拉丁詞彙suspiciosus，由suspicere (意為“懷疑”或“猜測”) 衍生而來。表示懷疑或猜疑的，對某事物存在疑心的感覺。",
     },
     "allegation": {
         "範圍": "8B",
         "全部": "All",
         "中文": "斷言/申述",
-        "例句": "The police are investigating the allegations of corruption.",
-        "句子的中文": "警方正在調查貪污的指控。"
+        "單字來由解釋": "allegation (名詞) 源自拉丁詞彙allegatio，由ad- (意為“向”) 和legare (意為“宣稱”或“斷言”) 組成。表示一種宣稱或斷言，通常指涉及某人或某事的陳述或聲明。",
     },
     "motive": {
         "範圍": "8B",
         "全部": "All",
         "中文": "動機/主旨",
-        "例句": "The detective tried to determine the motive behind the crime.",
-        "句子的中文": "警探試圖找出這起犯罪背後的動機。"
+        "單字來由解釋": "motive (名詞) 源自拉丁詞彙motivus，由movere (意為“移動”或“驅使”) 衍生而來。表示驅使人行動的原因、動機或目的。",
     },
     "candidate": {
         "範圍": "8B",
         "全部": "All",
         "中文": "候選人/候補者",
-        "例句": "There are several candidates running for mayor in the upcoming election.",
-        "句子的中文": "在即將到來的選舉中，有幾位候選人競選市長職位。"
+        "單字來由解釋": "candidate (名詞) 源自拉丁詞彙candidatus，意為“穿著白袍的人”。在古羅馬時代，競選官職的候選人會穿著白色袍子以示純潔。表示參選某個職位或地位的人。",
     },
     "in terms of": {
         "範圍": "8B",
         "全部": "All",
         "中文": "就...方面來說",
-        "例句": "In terms of salary, this job offers competitive pay.",
-        "句子的中文": "就薪資而言，這份工作提供具有競爭力的薪酬。"
+        "單字來由解釋": "in terms of (片語) 表示從某個特定的角度或方面來看待或評估某事物。",
     },
     "suffocation": {
         "範圍": "8B",
         "全部": "All",
         "中文": "窒息/悶死",
-        "例句": "The victim died of suffocation due to lack of oxygen.",
-        "句子的中文": "受害者因缺氧而窒息致死。"
+        "單字來由解釋": "suffocation (名詞) 源自拉丁詞彙suffocare，由sub- (意為“下面”) 和focare (意為“窒息”或“使窒息”) 組成。表示窒息或因無法呼吸而導致窒息的狀態。",
     },
     "deprived (of)": {
         "範圍": "8B",
         "全部": "All",
         "中文": "被剝奪了",
-        "例句": "Children in poverty often grow up deprived of basic necessities.",
-        "句子的中文": "貧困中的孩子通常在缺乏基本生活必需品的環境中長大。"
+        "單字來由解釋": "deprived (形容詞) 源自拉丁詞彙deprivare，由de- (意為“從...中剝奪”) 和privare (意為“剝奪”或“失去”) 組成。表示被剝奪或喪失某事物，通常指基本需求或權利。",
     },
     "remorse": {
         "範圍": "8B",
         "全部": "All",
         "中文": "痛悔/自責",
-        "例句": "He felt deep remorse for his actions and apologized sincerely.",
-        "句子的中文": "他對自己的行為感到深深的懊悔，並誠摯地道歉。"
+        "單字來由解釋": "remorse (名詞) 源自拉丁詞彙remorsus，由remordere (意為“咬”或“折磨”) 組成。表示對自己的行為或決定感到痛悔、後悔或自責的情感。",
     },
     "manipulative": {
         "範圍": "8B",
         "全部": "All",
         "中文": "操作的/用手控制的/巧妙處理的",
-        "例句": "She is known for her manipulative tactics in getting what she wants.",
-        "句子的中文": "她以巧妙的策略來獲得自己想要的東西而聞名。"
+        "單字來由解釋": "manipulative (形容詞) 源自拉丁詞彙manipulus，意為“操縱”或“操作”。表示以巧妙或不正當的手段來控制、影響或操縱他人或情況。",
     },
     "frankly": {
         "範圍": "8B",
         "全部": "All",
         "中文": "率直地/坦白地",
-        "例句": "To be Frank, I don't think your idea will work.",
-        "句子的中文": "坦白地說，我不認為你的想法會奏效。"
+        "單字來由解釋": "frankly (副詞) 源自法語詞彙franc，意為“坦率”或“直言不諱”。表示以直率、坦白或毫不保留的方式表達意見、感受或想法。",
     },
     "encounter": {
         "範圍": "9A",
         "全部": "All",
         "中文": "遭遇/遇到",
-        "例句": "I had a strange encounter with a mysterious person last night.",
-        "句子的中文": "昨晚我與一個神秘人有了一次奇怪的遭遇。"
+        "單字來由解釋": "encounter (動詞) 源自拉丁詞彙incontrare，由in- (意為“進入”) 和contra- (意為“對抗”) 組成。表示偶然或意外地遇到某人或某事物，或經歷某種情況或事件。",
     },
     "memorial": {
         "範圍": "9A",
         "全部": "All",
         "中文": "紀念物/紀念碑",
-        "例句": "They built a memorial to honor the soldiers who died in the war.",
-        "句子的中文": "他們建造了一個紀念物，以紀念在戰爭中犧牲的士兵。"
+        "單字來由解釋": "memorial (名詞) 源自拉丁詞彙memorialis，由memor (意為“記憶”或“紀念”) 組成。表示為紀念某人或某個事件而設立的物品、建築物或碑文。",
     },
     "stunned": {
         "範圍": "9A",
         "全部": "All",
         "中文": "驚倒/驚歎",
-        "例句": "I was stunned by the breathtaking beauty of the sunset.",
-        "句子的中文": "夕陽的美麗令我驚嘆不已。"
+        "單字來由解釋": "stunned (形容詞) 源自古英語詞彙stunne，意為“打昏”或“使震驚”。表示因驚訝、驚愕或震驚而暫時失去反應或能力。",
     },
     "firsthand": {
         "範圍": "9A",
         "全部": "All",
         "中文": "第一手的/直接的",
-        "例句": "I got firsthand information about the incident from a reliable source.",
-        "句子的中文": "我從一個可靠的來源獲得了關於這一事件的第一手信息。"
+        "單字來由解釋": "firsthand (形容詞) 源自手部動詞hand，加上first (意為“第一”) 。表示直接從第一個人或來源獲得的資訊、經驗或觀察。",
     },
     "carving": {
         "範圍": "9A",
         "全部": "All",
         "中文": "雕刻/雕刻術",
-        "例句": "The artist spent months working on the intricate carving of the statue.",
-        "句子的中文": "這位藝術家花了幾個月的時間來完成這座雕像上複雜的雕刻工作。"
+        "單字來由解釋": "carving (名詞) 源自動詞carve，意為“刻、雕刻”。表示使用刀、工具或其他尖銳的物體在材料上刻、雕刻或削去部分物質以形成藝術品、裝飾品或細節。",
     },
     "immortalize": {
         "範圍": "9A",
         "全部": "All",
         "中文": "使永恆/使不滅",
-        "例句": "His famous speech immortalized him in the hearts of the people.",
-        "句子的中文": "他那著名的演講使他在人們心中永垂不朽。"
+        "單字來由解釋": "immortalize (動詞) 源自immortal (意為“不朽的”) 和-ize (動詞化後綴) 。表示使某事物永遠存在、流傳下去或成為不朽的，通常指以某種方式記錄、描繪或紀念某人或某事物。",
     },
     "gaze": {
         "範圍": "9A",
         "全部": "All",
         "中文": "凝視/注視",
-        "例句": "She gazed out of the window, lost in her own thoughts.",
-        "句子的中文": "她凝視著窗外，陷入自己的思緒之中。"
+        "單字來由解釋": "gaze (動詞) 源自中古英語詞彙gasen，意為“看”或“注視”。表示集中目光、凝視或專注地看某人、某物或某個方向。",
     },
     "garment": {
         "範圍": "9A",
         "全部": "All",
         "中文": "衣服/服裝",
-        "例句": "She designed a stunning garment for the fashion show.",
-        "句子的中文": "她為時裝秀設計了一件令人驚艷的服裝。"
+        "單字來由解釋": "garment (名詞) 源自古英語詞彙gārment，意為“裝束”或“外衣”。表示人們穿在身上用來保護身體、遮蔽或裝飾的物品，包括衣服、服裝或外衣。",
     },
     "vandal": {
         "範圍": "9A",
         "全部": "All",
         "中文": "汪達爾人",
-        "例句": "The vandals destroyed several ancient monuments in the city.",
-        "句子的中文": "這些破壞者毀掉了城市裡的幾座古代紀念碑。"
+        "單字來由解釋": "vandal (名詞) 源自古地中海語詞彙Wandal，指的是一個古代日爾曼部落的成員。現代用法中，vandal表示一個破壞、糟蹋或毀壞公共財產、藝術品或其他價值物品的人。",
     },
     "virtual": {
         "範圍": "9A",
         "全部": "All",
         "中文": "事實上的/實際上的",
-        "例句": "She created a virtual world in her computer game.",
-        "句子的中文": "她在電腦遊戲中創造了一個虛擬世界。"
+        "單字來由解釋": "virtual (形容詞) 源自拉丁詞彙virtus，意為“品德”或“美德”。在現代用法中，virtual表示在事實上或實際上存在或發生，儘管可能不是物理上的或直接的。",
     },
     "empire": {
         "範圍": "9B",
         "全部": "All",
         "中文": "帝國/大企業",
-        "例句": "The Roman Empire was one of the most powerful empires in history.",
-        "句子的中文": "羅馬帝國是歷史上最強大的帝國之一。"
+        "單字來由解釋": "empire (名詞) 源自古法語詞彙empereor，意為“帝王”。表示由一個國家或組織統治的廣大領土、領域或影響範圍，也可以指一個巨大的企業或機構。",
     },
     "account": {
         "範圍": "9B",
         "全部": "All",
         "中文": "解釋/說明",
-        "例句": "He gave a detailed account of what happened that night.",
-        "句子的中文": "他對那天晚上發生的事情進行了詳細的解釋。"
+        "單字來由解釋": "account (名詞) 源自拉丁詞彙computare，意為“計算”或“估計”。表示關於某人、某事或某情況的陳述、描述、解釋或報告。",
     },
     "exception": {
         "範圍": "9B",
         "全部": "All",
         "中文": "例外/例外的人/除外/除去",
-        "例句": "Everyone has to follow the rules, without exception.",
-        "句子的中文": "每個人都必須遵守規則，沒有例外。"
+        "單字來由解釋": "exception (名詞) 源自拉丁詞彙exceptio，由ex- (意為“除去”) 和capere (意為“拿取”) 組成。表示與一般規則、慣例或情況不同或不符合的情況、事件或人。",
     },
     "take": {
         "範圍": "9B",
         "全部": "All",
         "中文": "接管/繼任",
-        "例句": "He will take over as CEO of the company next month.",
-        "句子的中文": "他將在下個月接任該公司的首席執行官。"
+        "單字來由解釋": "take (動詞) 源自古英語詞彙tacan，意為“拿取”或“抓住”。表示接收、接受或採取某物或某種狀態，也可以指接管、繼任或開始承擔某種職責、角色或地位。",
     },
     "historian": {
         "範圍": "9B",
         "全部": "All",
         "中文": "歷史學家",
-        "例句": "The historian wrote a comprehensive book on ancient civilizations.",
-        "句子的中文": "這位歷史學家寫了一本關於古代文明的綜合性書籍。"
+        "單字來由解釋": "historian (名詞) 由詞根history (意為“歷史”) 和字尾-ian (表示“與…相關的人”) 組成。指專門研究、記錄和解釋過去事件和時代的人，從事歷史學研究和撰寫。",
     },
     "cutting-edge": {
         "範圍": "9B",
         "全部": "All",
         "中文": "最前線/尖端",
-        "例句": "The company is known for its cutting-edge technology.",
-        "句子的中文": "這家公司以其尖端技術而聞名。"
+        "單字來由解釋": "cutting-edge (形容詞) 源自cutting (動詞) ，意為“切割”或“剪斷”，以及edge (名詞) ，意為“邊緣”。表示最新、最先進或最具創新性的，常用來形容科技、研究、技術等領域中最前沿和尖端的事物或觀念。",
     },
     "initial": {
         "範圍": "9B",
         "全部": "All",
         "中文": "開始的/最初的",
-        "例句": "I had some difficulties during the initial stage of the project.",
-        "句子的中文": "在項目的最初階段，我遇到了一些困難。"
+        "單字來由解釋": "initial (形容詞) 源自拉丁詞彙initialis，由initium (意為“開始”) 組成。表示最早、最初或開始的，用於描述某事物的開始階段、最初的狀態或最初的步驟。",
     },
     "spot": {
         "範圍": "9B",
         "全部": "All",
         "中文": "斑點/斑塊",
-        "例句": "There was a spot of paint on her new dress.",
-        "句子的中文": "她的新裙子上有一點油漆。"
+        "單字來由解釋": "spot (名詞) 源自古英語詞彙spott，意為“點”或“斑點”。表示一個小區域或地點，通常與周圍有所不同，可以是顏色、質地、形狀或其他特徵上的區別。",
     },
     "disrespectful": {
         "範圍": "9B",
         "全部": "All",
         "中文": "無禮的/失禮的",
-        "例句": "His disrespectful behavior towards the teacher will not be tolerated.",
-        "句子的中文": "他對老師的無禮行為將不被容忍。"
+        "單字來由解釋": "disrespectful (形容詞) 由詞根respect (意為“尊敬”) 和字首dis- (表示“相反”或“不”) 組成。表示缺乏尊重、不禮貌或對他人不尊敬的態度或行為。",
     },
     "forge": {
         "範圍": "9B",
         "全部": "All",
         "中文": "熔鐵爐/煉冶場",
-        "例句": "The blacksmith worked in the forge, shaping the hot metal.",
-        "句子的中文": "鐵匠在熔鐵爐中工作，塑造著熱金屬。"
-    }
+        "單字來由解釋": "forge (名詞) 源自古英語詞彙forgian，意為“打造”或“鍛造”。在此用法中，指一個用於加熱金屬並以鍛造、塑造或熔化的設備或場所，通常是用來製作金屬工藝品、工具或武器的場所。",
+    },
 }
